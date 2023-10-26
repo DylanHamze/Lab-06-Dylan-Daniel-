f216f3f0225aee6e948db5ae54b61b324ff0521e
DylanHamze <149001978+DylanHamze@users.noreply.github.com>
2023-10-26 17:07:42 -0400

    Delete 6.py

--- 6.py
+++
@@ -1,6 +0,0 @@
-def decoder(encoded_password):
-    decoded_password = ''
-    for digit in encoded_password:
-        old_number = str((int(digit) - 3) % 10)
-        decoded_password += old_number
-    return decoded_password
