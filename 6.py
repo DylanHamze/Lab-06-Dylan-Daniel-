4c369d0004fc5ca7c8b7aed0d0f155ac281e26ee
DylanHamze <dylan.hamze@ufl.edu>
2023-10-25 23:36:12 -0400

    A

--- 6.py
+++ 6.py
@@ -1,2 +1,6 @@
-print("hello")
-print("h")
+def decoder(encoded_password):
+    decoded_password = ''
+    for digit in encoded_password:
+        old_number = str((int(digit) - 3) % 10)
+        decoded_password += old_number
+    return decoded_password
